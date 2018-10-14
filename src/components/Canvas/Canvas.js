4c627212c45097e6f6c95e063eca05fce8b3964c
SukachevAlex <sukachevalexandr@gmail.com>
2018-10-14 12:38:16 +0300

    Добавил возможность включения звука под видео
    При развертывании видео на всю ширину появляется иконка динамика. Можно включить или выключить звук.

--- src/components/Canvas/Canvas.js
+++ src/components/Canvas/Canvas.js
@@ -2,12 +2,13 @@ import { Camera } from '../Camera/Camera';
 
 const Canvas = [];
 const controlls = document.querySelector('.camera__controlls');
+const volumeControll = document.querySelector('.volume__controll');
 
 (function() {
     for (let i = 0; i < Camera.length; i++) {
         const canvas = document.createElement('canvas');
         const ctx = canvas.getContext('2d');
-        canvas.className = `camera camera-${i+1}`;
+        canvas.className = `camera camera-${i+1} camera_muted`;
         canvas.width = 640;
         canvas.height = 480;
         ctx.filter = `
@@ -18,11 +19,15 @@ const controlls = document.querySelector('.camera__controlls');
         canvas.addEventListener('click', (e) => {
             canvas.classList.toggle('camera_open');
             controlls.classList.toggle('camera__controlls_visible');
+            canvas.classList.add('camera_muted');
+            volumeControll.classList.add('volume__controll_inactive');
         });
 
         const animate = () => {
             ctx.drawImage(Camera[i], 0, 0, canvas.width, canvas.height);
             window.requestAnimationFrame(animate);
+
+            canvas.classList.contains('camera_muted') ? Camera[i].muted = true : Camera[i].muted = false;
         }
         animate();
 
--- src/monitoring.js
+++ src/monitoring.js
@@ -6,6 +6,8 @@ const cameraBackBtn = document.querySelector('.camera__btn');
 const cameraControlls = document.querySelector('.camera__controlls');
 const brightnessControll = document.querySelector('.range-controll_brightness');
 const contrastControll = document.querySelector('.range-controll_contrast');
+const volumeControll = document.querySelector('.volume__controll');
+
 (function() {
     for (let i = 0; i < Canvas.length; i++) {
         cameraList.appendChild(Canvas[i]);
@@ -13,6 +15,7 @@ const contrastControll = document.querySelector('.range-controll_contrast');
 
     cameraBackBtn.addEventListener('click', function() {
         const camera = document.querySelector('.camera_open');
+        camera.classList.add('camera_muted');
         camera.classList.remove('camera_open');
         cameraControlls.classList.remove('camera__controlls_visible');
     });
@@ -21,16 +24,18 @@ const contrastControll = document.querySelector('.range-controll_contrast');
         const canvas = document.querySelector('.camera_open');
         const ctx = canvas.getContext('2d');
         ctx.filter = `brightness(${this.value / 100})`;
-
-        console.log(this.value);
     });
 
     brightnessControll.addEventListener('input', function() {
         const canvas = document.querySelector('.camera_open');
         const ctx = canvas.getContext('2d');
         ctx.filter = `contrast(${this.value / 100})`;
-
-        console.log(this.value);
     });
+
+    volumeControll.addEventListener('click', function() {
+        this.classList.toggle('volume__controll_inactive');
+        const canvas = document.querySelector('.camera_open');
+        canvas.classList.toggle('camera_muted');
+    })
 
 })();
